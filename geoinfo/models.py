c06380e58bd3b2ac106a05209fa5015d141cfe67
Aleksandr <popkov.aleks.v@yandex.ru>
2022-12-05 13:58:19 +0500

    extra empty line deleted

--- geoinfo/models.py
+++ geoinfo/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.utils import timezone
 
 class PlaceCoordinates(models.Model):
-
     place = models.CharField(max_length=200,
         unique=True,
         verbose_name='Место')
