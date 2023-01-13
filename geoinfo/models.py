a552fa84c8b866153ab7a546a52c4af5fa714c7a
Aleksandr <popkov.aleks.v@yandex.ru>
2023-01-13 15:25:25 +0500

    change verbose_name

--- geoinfo/models.py
+++ geoinfo/models.py
@@ -4,16 +4,13 @@ from django.utils import timezone
 class PlaceCoordinates(models.Model):
     place_name = models.CharField(max_length=200,
         unique=True,
-        verbose_name='Место')
-
+        verbose_name='Адрес')
     place_lon = models.FloatField(
         null=True,
         verbose_name='Долгота')
-
     place_lat = models.FloatField(
         null=True,
         verbose_name='Широта')
-
     last_update = models.DateTimeField(
         auto_now_add=True,
         verbose_name='Последнее обновление')
